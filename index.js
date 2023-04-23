201f89a5efcfc1df59d05cf97cd20cfc01ebd549
unknown <gardettefaustin@gmail.com>
2023-04-23 10:05:12 +0200

    todolist

---
+++ index.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+
+function storeList() {
+  window.localStorage.todolist = list.innerHTML;
+}
+
+function getTodo() {
+  if (window.localStorage.todolist) {
+    list.innerHTML = window.localStorage.todolist;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
+  }
+}
+
+getTodo();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+
+  storeList();
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
